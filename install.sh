29c6c9c3a64d5339917acf0f4850a1f5c9d01162
Alberto Corona <albcoron@gmail.com>
2013-11-17 10:26:37 -0600

    Adding to install script

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+DIR=$(dirname $0)
 XMDIR=$HOME/.xmonad
 
 if [ -f $XMDIR/xmonad.hs ]
 then
-    echo "Backing up xmonad.hs to xmonad.bak"
-    cp $XMDIR/xmonad.hs $XMDIR/xmonad.bak
+    echo "Copy xmonad.hs and backup current xmonad.hs? [y/n]"
+    read ANS
+    if [[ $ANS == 'y' || $ANS == 'Y' ]]
+    then
+        echo "Backing up xmonad.hs to xmonad.bak"
+        mv -v $XMDIR/xmonad.hs $XMDIR/xmonad.bak
+        cp -v $DIR/xmonad.hs $XMDIR
+        xmonad --recompile
+    fi
 fi
-
-cp xmonad.hs $XMDIR
