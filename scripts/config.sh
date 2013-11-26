d7a5a0abc478be26a926055a4de22accda448fc9
Alberto Corona <albcoron@gmail.com>
2013-11-25 22:30:21 -0600

    Fixed script bar backgrounds and fonts
    Signed-off-by: Alberto Corona <albcoron@gmail.com>

--- scripts/config.sh
+++ scripts/config.sh
@@ -12,12 +12,10 @@ if [[ -z `xrandr | grep " connected" | grep 'DVI-1'` ]]; then
 	XOFFSET="0"
 fi
 
-#FONT="-artwiz-lime-medium-r-normal-*-10-110-75-75-m-50-iso8859-*"
-FONT="-artwiz-limey-medium-r-normal-*-10-110-75-75-m-50-iso8859-*"
-#FONT="-*-lemon-*-*-*-*-*-*-*-*-*-*-*-*"
-white0="#93A1A1"
+FONT="-*-terminus-*-r-normal-*-*-80-*-*-*-*-iso8859-*"
 
-bar_bg="#000000"
+white0="#93A1A1"
+bar_bg="#586e75"
 bar_fg="#93a1a1"
 notify="#F39D21"
 warning="#D23D3D"
